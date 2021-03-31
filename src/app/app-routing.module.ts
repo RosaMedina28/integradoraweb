656d21fde361a1ef2db642388a5dcab6e10dd692
caricatura25 <19170101@uttcampus.edu.mx>
2021-03-31 15:44:55 -0600

    xd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { LoginComponent } from './Component/login/login.component';
 import { RegistroComponent } from './Component/registro/registro.component';
 import { NavBarComponent} from './Component/nav-bar/nav-bar.component';
 import { Error404Component } from './Component/error404/error404.component'
+import { seg2 } from './seg2.guard';
 
 const routes: Routes = [
   
     {path:'navbar', component:NavBarComponent},
     {path:'carrusel', component:CarruselComponent},
     {path:'navadmin', component:AdminnavComponent},
-    {path:'login', component:LoginComponent, pathMatch: 'full'},
+    {path:'login', component:LoginComponent, pathMatch: 'full',canActivate: [seg2]},
     {path:'registro', component:RegistroComponent},
     {path:'**',component:Error404Component}
 
