ceb36a716dc8813bdbe5a97939f4a59b0c605ef9
RosaMedina28 <rosamedina1008@gmail.com>
2021-03-31 12:00:38 -0600

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminnavComponent } from './Component/adminnav/adminnav.component';
 import { CarruselComponent } from './Component/carrusel/carrusel.component';
+import { LoginComponent } from './Component/login/login.component';
 
 import { NavBarComponent} from './Component/nav-bar/nav-bar.component';
 
 const routes: Routes = [
   
     {path:'navbar', component:NavBarComponent},
-    {path:'carrusel', component:CarruselComponent}
+    {path:'carrusel', component:CarruselComponent},
+    {path:'navadmin', component:AdminnavComponent},
+    {path:'login', component:LoginComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { SplashComponent } from './Component/splash/splash.component';
 import { CarruselComponent } from './Component/carrusel/carrusel.component';
 import { NavBarComponent } from './Component/nav-bar/nav-bar.component';
+import { AdminnavComponent } from './Component/adminnav/adminnav.component';
+import { LoginComponent } from './Component/login/login.component';
 
 
 @NgModule({
@@ -14,7 +16,9 @@ import { NavBarComponent } from './Component/nav-bar/nav-bar.component';
     AppComponent,
     NavBarComponent,
     SplashComponent,
-    CarruselComponent
+    CarruselComponent,
+    AdminnavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
